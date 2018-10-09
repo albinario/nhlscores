b036985455d83060f1ebe599aa9c23fed862e7cf
Albin Lindeborg <albin.lindeborg@protonmail.com>
2018-10-09 15:00:32 +0100

    remove comments

--- src/util/GamesFeed.js
+++ src/util/GamesFeed.js
@@ -10,17 +10,10 @@ const GamesFeed = {
          return response.json();
        }).then(jsonResponse => {
          if (jsonResponse) {
-           //console.log(jsonResponse);
            return jsonResponse.games.filter(ps => ps.schedule.playedStatus === "COMPLETED").map(game => {
-             // const homeTeamId = game.schedule.homeTeam.id;
-             // const homeTeamArr = homeTeamId-1;
-             // console.log(homeTeamArr);
-             //console.log(jsonResponse.references.teamreferences.id[homeTeamId-1].name);
-             //const homeTeamName = jsonResponse.references.teamreferences.find(id => id.id === homeTeamId);
              return {
                gameId: game.schedule.id,
                homeTeam: game.schedule.homeTeam.abbreviation,
-               //homeTeam: jsonResponse.references.teamreferences.id[homeTeamId-1].name,
                homeScore: game.score.homeScoreTotal,
                awayTeam: game.schedule.awayTeam.abbreviation,
                awayScore: game.score.awayScoreTotal
