fb3eec9f77fa66d7fe83ac2b9cdea0ddb0fb1a3d
Albin Lindeborg <albin.lindeborg@protonmail.com>
2018-10-22 19:41:56 +0100

    Remove comment

--- src/util/MainFeed.js
+++ src/util/MainFeed.js
@@ -42,7 +42,6 @@ const MainFeed = {
             awayGoalies.push(goalies.awayGoalies);
           });
         }
-        console.log(goals);
         return {
           gameId: game.schedule.id,
           homeTeamId: homeTeamInfo[0].id,
