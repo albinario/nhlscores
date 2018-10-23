f9e1ad89d2d54f50ff2f6074fa14b92066a28c76
Albin Lindeborg <albin.lindeborg@protonmail.com>
2018-10-23 14:55:29 +0100

    Fix dot issue in Goal.js

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -5,7 +5,7 @@ import Logos from '../../util/Logos';
 class Goal extends Component {
   render() {
     console.log("Goal: render()");
-    const playDesc = this.props.goal.playDescription.replace("Goal scored by", "").replace("(Empty Net)", "").replace(", assisted by ", " (").replace(" and", ",").replace(".", "").replace("(unassisted)", "(unassisted").replace("Shootout attempt by ", "(PS: ").replace(", scored!", "")+")";
+    const playDesc = this.props.goal.playDescription.replace("Goal scored by", "").replace("(Empty Net)", "").replace(", assisted by ", " (").replace(" and", ",").replace(/\./g, "").replace("(unassisted)", "(unassisted").replace("Shootout attempt by ", "(PS: ").replace(", scored!", "")+")";
     return (
       <div className="row">
         <div className="col-xs-1 text-center">{this.props.homeScore}-{this.props.awayScore}</div>
